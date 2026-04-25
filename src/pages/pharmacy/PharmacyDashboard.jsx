fec72a039cf8a36d3fecb38849a5461f6723d851
Arupyaa <amkeshk98@gmail.com>
2026-04-25 20:24:31 +0300

    routed pharmacy dashboard to main.jsx

--- src/pages/pharmacy/PharmacyDashboard.jsx
+++ src/pages/pharmacy/PharmacyDashboard.jsx
@@ -4,9 +4,9 @@ import PDashboardMain from "../../features/pharmacy/components/PDashboardMain";
 export default function PharmacyDashboard() {
     return (
         //main container 12-col system
-        <div className="grid grid-cols-12 gap-x-5">
+        <>
             <RetractableSidebar/>
             <PDashboardMain/>
-        </div>
+        </>
     )
 }
