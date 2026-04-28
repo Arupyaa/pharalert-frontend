4eff0c80a7bce5a6ce3315640f1ee962c99f644b
Arupyaa <amkeshk98@gmail.com>
2026-04-28 19:58:07 +0300

    refactored the layout of the pharmacydashboard

--- src/features/pharmacy/components/PDashboardMain.jsx
+++ src/features/pharmacy/components/PDashboardMain.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 export default function PDashboardMain() {
     return (
-        <div className='flex-1 bg-neutral-secondary'>
+        <div className='flex-1 bg-neutral-secondary w-full'>
             {/* navbar containing account name and avatar */}
-            <div className='w-full bg-neutral-main h-[60px] shadow-sm'></div>
+            
         </div>
     )
 }
