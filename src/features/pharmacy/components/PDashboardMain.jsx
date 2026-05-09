9b7f45e0e495b8da1331a87fd47224fba29dda5d
Ahmed Aboelazm <a336037@gmail.com>
2026-05-09 20:05:03 +0300

    this is cards

--- src/features/pharmacy/components/PDashboardMain.jsx
+++ src/features/pharmacy/components/PDashboardMain.jsx
@@ -9,12 +9,26 @@ import Select from '../../../components/shared/SelectMenu'
 import SelectMenu from '../../../components/shared/SelectMenu'
 import TabsLinks from '../../../components/shared/RetractableSidebar/Tabslink'
 import Badge from '../../../pages/pharmacy/Badge'
+import Card from '../../../pages/pharmacy/Card'
+
+
 
 export default function PDashboardMain() {
     return (
         <div className='flex-1 bg-neutral-secondary w-full flex h-full justify-center items-center'>
-            {/* navbar containing account name and avatar */}
-            <Badge/>
+             <Card
+        name="Pharalert Pharmacy"
+        address="Cairo"
+        image="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?q=80&w=1200&auto=format&fit=crop"
+        isOpen={false}
+        latitude={30.0384}
+        longitude={31.2101}
+
+        
+      />
+
+
+    
         </div>
     )
 }
